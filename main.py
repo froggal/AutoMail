4c8d9c407157c7ef4ac3f67b0c2d615baad100d7
froggal <keyfrog@outlook.kr>
2023-12-16 13:25:48 +0900

    env 설정

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import dotenv
+import os
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file)
+
+pw = os.environ("PASSWORD")
+send = os.environ("SENDER")
+receive = os.environ("RECEIVER")
