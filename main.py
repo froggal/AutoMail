513d0fd1436c138e5445fee648092ec27cf1bcb2
froggal <keyfrog@outlook.kr>
2023-12-16 13:36:27 +0900

    Add comments to all code, include SMTP configuration section

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+# import modules
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import dotenv
 import os
+
+# load dotenv
 dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
 
-pw = os.environ("PASSWORD")
-send = os.environ("SENDER")
-receive = os.environ("RECEIVER")
+# load account/server
+pw = os.environ(["PASSWORD"])
+send = os.environ(["SENDER"])
+receive = os.environ(["RECEIVER"])
+
+# set up SMTP
+smtp = smtplib.SMTP(os.environ["SERVER"], os.environ["PORT"])
+smtp.ehlo()
+smtp.starttls()
+smtp.login(send, pw)
